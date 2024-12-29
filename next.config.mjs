4f4a6a2d673cf307a302231db6a13a0adc3869a6
Hector Antonio Briceño Clemente <hector.a.briceno@ucv.ve>
2024-12-29 03:18:18 -0400

    Configurating the store for cloudinary images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["res.cloudinary.com"], // Permite el dominio de Cloudinary
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+				pathname: "/dzg6yhqzx/**", // Configura el patrón de ruta de Cloudinary
+			},
+		],
+	},
+};
 
 export default nextConfig;
