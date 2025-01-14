dcfef9e39c4cc85b9ac767b75ea3793142e2b03c
Hector Antonio Briceño Clemente <hector.a.briceno@ucv.ve>
2025-01-14 02:32:20 -0400

    Configurating productlist part 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["res.cloudinary.com"], // Permite el dominio de Cloudinary
+		domains: ["res.cloudinary.com", "cdn.bootstrapstudio.io"], // Permite el dominio de Cloudinary
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "res.cloudinary.com",
 				pathname: "/dzg6yhqzx/**", // Configura el patrón de ruta de Cloudinary
 			},
+
 		],
 	},
 };
--- src/app/productslist/page.jsx
+++ src/app/productslist/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-
+import Image from "next/image";
+import Link from "next/link";
 const ProductsList = () => {
     const cards = Array(9).fill({
         imgSrc: "https://cdn.bootstrapstudio.io/placeholders/1400x800.png",
@@ -15,16 +15,19 @@ const ProductsList = () => {
                 {cards.map((card, index) => (
                     <div className="col" key={index}>
                         <div>
-                            <img
-                                className="rounded img-fluid d-block w-100 fit-cover"
-                                style={{ height: "200px" }}
-                                src={card.imgSrc}
-                                alt={card.title}
-                            />
-                            <div className="py-4">
-                                <h4>{card.title}</h4>
-                                <p>{card.description}</p>
-                            </div>
+                            <Link href="/product">
+                                <Image
+                                    className="rounded img-fluid d-block w-100 fit-cover"
+                                    width={1400}
+                                    height={800}
+                                    src={card.imgSrc}
+                                    alt={card.title}
+                                />
+                                <div className="py-4">
+                                    <h4>{card.title}</h4>
+                                    <p>{card.description}</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 ))}
